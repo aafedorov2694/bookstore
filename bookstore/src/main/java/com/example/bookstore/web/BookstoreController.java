50b081247c3ff1f048839cd1e2f024414357729a
aafedorov2694 <antonfedorov2694@gmail.com>
2020-10-04 15:36:15 +0300

    Spring Security part1

--- bookstore/src/main/java/com/example/bookstore/web/BookstoreController.java
+++ bookstore/src/main/java/com/example/bookstore/web/BookstoreController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -69,7 +70,7 @@ public class BookstoreController {
 	}
 
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-	
+	@PreAuthorize("hasRole('ADMIN')")
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		
 		repository.deleteById(bookId);
